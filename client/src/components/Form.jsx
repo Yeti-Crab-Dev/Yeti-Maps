118898a1bd0180376b9f64a887b10fbe457becaa
Diana Li <dianali9619@gmail.com>
2022-02-18 20:49:03 -0500

    boilerplate for front-end and back-end

---
+++ client/src/components/Form.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+const Form = () => {
+
+    const [location, setLocation] = useState('');
+    const [post, setPost] = useState('');
+    
+    
+    return (
+        <div>
+
+        </div>
+    )
+}
---
+++ server/api.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const FromController = require('./controllers/...');
+
+const router = express.Router();
+
+
+
+module.exports = router;
