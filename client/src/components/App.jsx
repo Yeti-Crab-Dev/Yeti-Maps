0b1bd07caf5c96273a29cf06a1693eae0e2ad40c
Diana Li <dianali9619@gmail.com>
2022-02-19 19:24:59 -0500

    connected

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Form } from './Form.jsx'
+import GoogleMap from './GoogleMap'
 
 const App = () => {
     return (
         <div>
             <h1>This is a Test</h1>
             <Form />
+            <GoogleMap />
         </div>
     )
 
