36835d634c20cc6288610e3ad03f7d6babc32e96
Anthony <adavid16967@gmail.com>
2022-02-21 21:55:59 -0500

    delete working update not yet

--- client/src/components/ShowPost.jsx
+++ client/src/components/ShowPost.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 
 const ShowPost = () => {
 
     const [comments, setComments] = useState([]);
-
 
     useEffect(() => {
         getAllComment();
@@ -20,7 +20,30 @@ const ShowPost = () => {
             console.log(err.message)
         }
     }
-  
+
+    const deleteComment = async (id) => {
+        // console.log('in delete command')
+        console.log(id)
+        try {
+            await fetch(`http://localhost:3000/api/comments/${id}`, {
+                method: 'DELETE',
+            })
+            setComments(comments.filter(el => el.comments_id !== id))
+        } catch (error) {
+            console.error(error)
+        }
+            
+    }
+        
+  const upDateComment = async(id) => {
+    try {
+        await fetch(`http://localhost:3000/api/comments/${id}`, {
+            method: 'PUT',
+        })
+    } catch (error) {
+        console.error(error)
+    }
+  }
 
     return (
         <div>
@@ -33,6 +56,28 @@ const ShowPost = () => {
                             <td>{comment.country}</td>
                             <td>{comment.comment}</td>
                             <td>{comment.users}</td>
+                            <button onClick={() => deleteComment(comment.comment_id)}>Delete</button>
+
+                            <button type="button" data-toggle="modal" data-target="#myModal">Edit</button>
+
+                            <div id="myModal" class="modal fade" role="dialog">
+                            <div class="modal-dialog">
+
+                                <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                    <h4 class="modal-title">Edit Comment</h4>
+                                </div>
+                                <div class="modal-body">
+                                    <input type="text" className='form-control'/>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                </div>
+                                </div>
+
+                            </div>
+                            </div>
                         </tr>
                     )}
                 </tbody>
