226274f0b3fd0ef9e6ac2f8266f273f5a5192cb8
Diana Li <dianali9619@gmail.com>
2022-04-16 18:16:44 -0400

    implemented bcrypt Co-authored-by: Michelle Shahid shahidmichelle@gmail.com

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,21 +1,48 @@
 const db = require('../models/dbModel');
+const bcrypt = require('bcryptjs');
+const saltRounds = 10;
 
 const userController = {};
 
-userController.createUser = async (req,res,next)=>{
+userController.hasher = async (req, res, next) => {
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashPW = await bcrypt.hash(req.body.password, salt);
+        res.locals.password = hashPW
+        res.locals.username = req.body.username
+        res.locals.name = req.body.name
+        next();
+    } catch (err) {
+        return console.log(err)
+    }
+}
+
+userController.createUser = async (req, res, next) => {
     console.log('In create user')
-    try{
-        const {name, username, password} = req.body;
-        console.log(req.body);
-        const query = 'INSERT INTO users (name,username, password ) VALUES ( $1,$2,$3)';
-        const result = await db.query(query,[name,username,password]);
+    try {
+        const { name, username, password } = res.locals;
+        console.log('res.locals in create user', res.locals);
+
+        const queryA = `INSERT INTO users (name, username, password) VALUES ($1,$2,$3) RETURNING *`;
+        console.log('insert yeah?')
+        const result = await db.query(queryA, [name, username, password]);
+        // res.json(result.rows[0]);
+
+        console.log('number 2')
+
         const queryGetUser = 'SELECT * FROM users WHERE username = $1';
-        const result2 = await db.query(queryGetUser,[username]);
-        console.log(result2.rows[0]);
+
+        console.log('number 3')
+
+        const result2 = await db.query(queryGetUser, [username]);
+
+        console.log('hello', result2.rows[0]);
+
         res.locals.id = result2.rows[0].user_id;
+
         return next();
     }
-    catch(err){
+    catch (err) {
         return next({
             log: 'Error in userController.createUser',
             message: 'Cant create user!'
@@ -23,22 +50,22 @@ userController.createUser = async (req,res,next)=>{
     }
 };
 
-userController.getUserWithLogin = async (req,res,next)=>{
-    try{
-        const {username, password} = req.body;
+userController.getUserWithLogin = async (req, res, next) => {
+    try {
+        const { username, password } = req.body;
         const query = `SELECT * from users WHERE username = $1 AND password = $2`;
-        const response = await db.query(query,[username,password]);
+        const response = await db.query(query, [username, password]);
         res.locals.status = {};
 
-        if(!response.rows.length){
+        if (!response.rows.length) {
             res.locals.status.OK = false;
-        } else{
+        } else {
             res.locals.status.OK = true;
             res.locals.status.user = response.rows[0];
         }
         next();
     }
-    catch(err){
+    catch (err) {
         return next({
             log: `Error in userController.getUser : ${err.message}`,
             message: 'Cant get user!'
@@ -46,16 +73,16 @@ userController.getUserWithLogin = async (req,res,next)=>{
     }
 };
 
-userController.getUser = async (req,res,next)=>{
-    try{
-        const {id} = req.params;
+userController.getUser = async (req, res, next) => {
+    try {
+        const { id } = req.params;
         const query = `SELECT * from users WHERE user_id = $1`;
-        const response = await db.query(query,[id]);
-        if(!response.rows.length) throw new Error ('no user detected');
+        const response = await db.query(query, [id]);
+        if (!response.rows.length) throw new Error('no user detected');
         res.locals.user = response.rows[0];
         next();
     }
-    catch(err){
+    catch (err) {
         return next({
             log: 'Error in userController.getUser',
             message: 'Cant get user!'
@@ -63,15 +90,15 @@ userController.getUser = async (req,res,next)=>{
     }
 };
 
-userController.getAllUsers = async (req,res,next)=>{
-    try{
-        const {id} = req.params;
+userController.getAllUsers = async (req, res, next) => {
+    try {
+        const { id } = req.params;
         const query = `SELECT * from users`;
         const response = await db.query(query);
         res.locals.users = response.rows;
         next();
     }
-    catch(err){
+    catch (err) {
         return next({
             log: 'Error in userController.getUser',
             message: 'Cant get user!'
@@ -79,15 +106,15 @@ userController.getAllUsers = async (req,res,next)=>{
     }
 };
 
-userController.updateUser = async (req,res,next)=>{
-    try{
-        const {name, username, password} = req.body;
+userController.updateUser = async (req, res, next) => {
+    try {
+        const { name, username, password } = req.body;
         const query = "UPDATE users SET name = $1, password = $2  WHERE username = $3"
-        const result = await db.query(query,[name,password,username]);
+        const result = await db.query(query, [name, password, username]);
         console.log(result);
         next();
     }
-    catch(err){
+    catch (err) {
         return next({
             log: 'Error in userController.updateUser',
             message: 'Cant update user!'
